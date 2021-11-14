9789be8e7681062e208690ba419008bf8843dea3
Waloc.Productions.1 <68969673+ninkea@users.noreply.github.com>
2021-11-14 14:45:50 +0000

    Add files via upload

---
+++ SuperMonka.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using HarmonyLib;
+using BepInEx;
+using UnityEngine;
+using System.Reflection;
+using UnityEngine.XR;
+using Photon.Pun;
+using System.IO;
+using System.Net;
+using Photon.Realtime;
+using UnityEngine.Rendering;
+
+namespace fly
+{
+    [BepInPlugin("org.XenomVR.monkeytag.SuperMonka", "SuperMonka", "VERSION 0.0.0.1")]
+    public class MyPatcher : BaseUnityPlugin
+    {
+        public void Awake()
+        {
+            var harmony = new Harmony("com.XenomVR.monkeytag.HOW TO USE");
+            harmony.PatchAll(Assembly.GetExecutingAssembly());
+        }
+    }
+
+    [HarmonyPatch(typeof(GorillaLocomotion.Player))]
+    [HarmonyPatch("Update", MethodType.Normal)]
+    public class Class1
+    {
+        static bool body = false;
+        static void Postfix(GorillaLocomotion.Player __instance)
+        {
+
+            if (!PhotonNetwork.CurrentRoom.IsVisible || !PhotonNetwork.InRoom)
+            {
+                List<InputDevice> list = new List<InputDevice>();
+                InputDevices.GetDevicesWithCharacteristics(UnityEngine.XR.InputDeviceCharacteristics.HeldInHand | UnityEngine.XR.InputDeviceCharacteristics.Right | UnityEngine.XR.InputDeviceCharacteristics.Controller, list);
+                list[0].TryGetFeatureValue(CommonUsages.gripButton, out body);
+
+                if (body)
+                {
+                    __instance.bodyCollider.attachedRigidbody.detectCollisions = false;
+                }
+
+            }
+        }
+    }
+}
